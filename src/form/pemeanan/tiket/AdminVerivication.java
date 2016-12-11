8025be73db0323e1822406f815a5feeb32b0fe81
Nasih Ahmad F <nasihahmad66.na@gmail.com>
2016-12-11 14:18:24 +0700

    -add validation

---
+++ src/form/pemeanan/tiket/AdminVerivication.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package form.pemeanan.tiket;
+
+/**
+ *
+ * @author NA
+ */
+public class AdminVerivication {
+    private String Adver;
+    
+    public String getAdver() {
+        return Adver;
+    }
+
+    public void setAdver(String Adver) {
+        this.Adver = Adver;
+    }
+    
+}
